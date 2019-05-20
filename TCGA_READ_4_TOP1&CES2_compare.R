64b340be3bfc8a2a7284d2bd389cac00bce97ba0
yongminlee <lym950@yonsei.ac.kr>
2019-05-20 14:28:34 +0000

    FDR filtered added to TCGA_READ

--- TCGA_READ_4_TOP1&CES2_compare.R
+++ TCGA_READ_4_TOP1&CES2_compare.R
@@ -20,3 +20,19 @@ p+ stat_compare_means() + stat_mean()
 
 p <- ggboxplot(data=subset, "CMS4", "CES2")
 p+ stat_compare_means()
+
+# Set FDR threshold:
+FDR.filter <- 0.2
+FDR.filtered.subset <- subset[subset[ ,"FDR"]<=0.2, ]
+
+CMS4.filtered <- FDR.filtered.subset[ ,"prediction"] == "CMS4"
+FDR.filtered.subset <- cbind(FDR.filtered.subset, CMS4.filtered)
+
+ggboxplot(data=FDR.filtered.subset, "prediction", "TOP1")
+ggboxplot(data=FDR.filtered.subset, "prediction", "CES2")
+
+p <- ggboxplot(data=FDR.filtered.subset, "CMS4.filtered", "TOP1")
+p+ stat_compare_means()
+
+p <- ggboxplot(data=FDR.filtered.subset, "CMS4.filtered", "CES2")
+p+ stat_compare_means()
